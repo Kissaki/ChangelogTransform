af27ed377646443e9efe50e5006ca561b09bd595
Jan Klass <kissaki@posteo.de>
2019-08-17 17:31:23 +0200

    Initial project with history read, store, categorization and html file generating
    time spent 4h

---
+++ changelog-transform/GitLog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace changelog_transform
+{
+    class GitLog
+    {
+        public DirectoryInfo RepoPath { get; }
+        public string Source { get; }
+        public string Target { get; }
+
+        public GitLog(DirectoryInfo di, string fromRef, string toRef)
+        {
+            RepoPath = di;
+            Source = fromRef;
+            Target = toRef;
+        }
+    }
+}
