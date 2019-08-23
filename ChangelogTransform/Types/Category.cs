a1cb8ada777d5898ac25233f041fe31694e9118b
Jan Klass <kissaki@posteo.de>
2019-08-24 00:40:38 +0200

    Code: Remove obsolete categories

--- ChangelogTransform/Types/Category.cs
+++ ChangelogTransform/Types/Category.cs
@@ -11,36 +11,8 @@
         Improvements,
         // Fixed bugs
         Bugfixes,
-        // Fixed issues or potential/theoretical issues
-        Hardening,
 
-        // Everything build scripts, build configuration, build job configuration
-        BuildInfrastructure,
         // Code formatting, being able to compile on different platforms, code improvements - could be split up further
         Code,
-        // Related to translation of Mumble and installer
-        Translation,
-        // Tests, test programs, src/tests
-        Tests,
-        ProtocolDocumentation,
-
-        Overlay,
-        PositionalAudio,
-        Installer,
-        Thirdparty,
-        Theme,
-        Grpc,
-        Ice,
-        Opus,
-        Bonjour,
-        Qt5,
-        GlobalShortcut,
-        BanList,
-        OSInfo,
-        G15Lcd,
-        Filter,
-        TextToSpeech,
-        Qt4,
-        SSL,
     };
 }
